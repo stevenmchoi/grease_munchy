68fb4fed186aea9afa402623ee9977c9058f5597
Steven Choi <steven.m.choi@gmail.com>
2018-04-03 16:00:16 -0700

    Fine-tune ApplicationController to handle redirecting and current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,27 +3,34 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :logged_in?
 
+  private
+
   def current_user
     return nil unless session[:session_token]
     @current_user ||= User.find_by_session_token(session[:session_token])
   end
 
   def login(user)
+    user.reset_session_token!
     session[:session_token] = user.session_token
+    @current_user = user
   end
 
   def logout
-    session[:session_token] = nil
     current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
   end
 
   def logged_in?
     !!current_user
   end
 
-  # def require_logged_out
-  # end
-  #
-  # def require_logged_in
-  # end
+  def redirect_logged_out
+    redirect_to api_users_url if logged_in?
+  end
+
+  def redirect_logged_in
+    redirect_to api_session unless logged_in?
+  end
 end
