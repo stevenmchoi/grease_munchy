f01e96250692d8a67f95f269c301cf918cf99cfc
Steven Choi <steven.m.choi@gmail.com>
2018-04-03 16:43:45 -0700

    Fill out Users and Session Controllers to test w/ ajax

--- app/controllers/api/session_controller.rb
+++ app/controllers/api/session_controller.rb
@@ -1,2 +1,24 @@
 class Api::SessionController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: 401
+    end
+  end
+
+  def destroy
+    if current_user
+      logout
+      render json: {}
+    else
+      render json: ['Not logged in!'], status: 404
+    end
+  end
 end
