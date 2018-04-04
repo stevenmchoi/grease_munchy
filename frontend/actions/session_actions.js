942b37b194f1c61fcf37b96d8ea64b93c77f0ee0
Steven Choi <steven.m.choi@gmail.com>
2018-04-04 15:44:02 -0700

    Clarify error array parameter & fix action creators

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -4,29 +4,29 @@ export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 
 export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
 
-const receiveCurrentUser = (currentUser) => {
+const receiveCurrentUser = (currentUser) => ({
     type: RECEIVE_CURRENT_USER,
     currentUser
-};
+});
 
-const receiveErrors = (errors) => {
+const receiveErrors = (errorsArr) => ({
     type: RECEIVE_ERRORS,
-    errors
-};
+    errors: errorsArr
+});
 
-export const login = user => dispatch =>
+export const login = (user) => (dispatch) =>
     SessionApiUtil.login(user).then(
         (user) => dispatch(receiveCurrentUser(user)),
-        (errors) => dispatch(receiveErrors(errors))
+        (errorsArr) => dispatch(receiveErrors(errorsArr))
     );
 
-export const logout = () => dispatch =>
+export const logout = () => (dispatch) =>
     SessionApiUtil.logout().then(
         (user) => dispatch(receiveCurrentUser(null)),
-        (errors) => dispatch(receiveErrors(errors))
+        (errorsArr) => dispatch(receiveErrors(errorsArr))
     );
-export const signup = (user) => dispatch =>
-    SessionApiUtil.signup().then(
+export const signup = (user) => (dispatch) =>
+    SessionApiUtil.signup(user).then(
         (user) => dispatch(receiveCurrentUser(user)),
-        (errors) => dispatch(receiveErrors(errors))
+        (errorsArr) => dispatch(receiveErrors(errorsArr))
     );
