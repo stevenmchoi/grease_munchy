9c5b79cee6544950e130066ad565ee85144a1d1a
Steven Choi <steven.m.choi@gmail.com>
2018-04-04 16:55:47 -0700

    Create all session reducers

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,32 +1,33 @@
-import * as SessionApiUtil from '../util/session_api_util';
+import * as SessionApiUtil from "../util/session_api_util";
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 
-export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 
-const receiveCurrentUser = (currentUser) => ({
-    type: RECEIVE_CURRENT_USER,
-    currentUser
+const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
 });
 
-const receiveErrors = (errorsArr) => ({
-    type: RECEIVE_ERRORS,
-    errors: errorsArr
+const receiveErrors = errorsArr => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors: errorsArr
 });
 
-export const login = (user) => (dispatch) =>
-    SessionApiUtil.login(user).then(
-        (user) => dispatch(receiveCurrentUser(user)),
-        (errorsArr) => dispatch(receiveErrors(errorsArr))
-    );
+export const signup = user => dispatch =>
+  SessionApiUtil.signup(user).then(
+    user => dispatch(receiveCurrentUser(user)),
+    errorsArr => dispatch(receiveErrors(errorsArr))
+  );
 
-export const logout = () => (dispatch) =>
-    SessionApiUtil.logout().then(
-        (user) => dispatch(receiveCurrentUser(null)),
-        (errorsArr) => dispatch(receiveErrors(errorsArr))
-    );
-export const signup = (user) => (dispatch) =>
-    SessionApiUtil.signup(user).then(
-        (user) => dispatch(receiveCurrentUser(user)),
-        (errorsArr) => dispatch(receiveErrors(errorsArr))
-    );
+export const login = user => dispatch =>
+  SessionApiUtil.login(user).then(
+    user => dispatch(receiveCurrentUser(user)),
+    errorsArr => dispatch(receiveErrors(errorsArr))
+  );
+
+export const logout = () => dispatch =>
+  SessionApiUtil.logout().then(
+    user => dispatch(receiveCurrentUser(null)),
+    errorsArr => dispatch(receiveErrors(errorsArr))
+  );
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { signup, login, logout } from "./actions/session_actions";
+import configureStore from "./store/store";
 
-document.addEventListener('DOMContentLoaded', () => {
-	const root = document.getElementById('root');
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  const store = configureStore();
 
-	// Test begin
-	// window.dispatch = dispatch
-	window.signup = signup
-	window.login = login
-	window.logout = logout
-	// Test end
+  // Test begin
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
-	ReactDOM.render(<h1>Food is better fried</h1>, root);
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
+  // Test end
+
+  ReactDOM.render(<h1>Food is better fried</h1>, root);
 });
