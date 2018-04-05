50bfb485fb9f01758b6e48738c9320f15c81a1b6
Steven Choi <steven.m.choi@gmail.com>
2018-04-05 14:12:31 -0700

    Create App and Root components with Routers

---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+	<div>
+		<h1>Grease Munchy...why not?</h1>
+	</div>
+);
+
+export default App;
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,20 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { signup, login, logout } from "./actions/session_actions";
-import configureStore from "./store/store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { signup, login, logout } from './actions/session_actions';
+import configureStore from './store/store';
+import Root from './components/root';
 
-document.addEventListener("DOMContentLoaded", () => {
-  const root = document.getElementById("root");
-  const store = configureStore();
+document.addEventListener('DOMContentLoaded', () => {
+	const root = document.getElementById('root');
+	const store = configureStore();
 
-  // Test begin
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+	// Test begin
+	window.getState = store.getState;
+	window.dispatch = store.dispatch;
 
-  window.signup = signup;
-  window.login = login;
-  window.logout = logout;
-  // Test end
+	window.signup = signup;
+	window.login = login;
+	window.logout = logout;
+	// Test end
 
-  ReactDOM.render(<h1>Food is better fried</h1>, root);
+	ReactDOM.render(<Root store={store} />, root);
 });
