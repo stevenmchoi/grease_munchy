7255afefc0624a7a942213f3f29078df8721818f
Steven Choi <steven.m.choi@gmail.com>
2018-04-05 18:01:18 -0700

    Test in forms from on; no need to use session_actions in console

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { signup, login, logout } from './actions/session_actions';
+// import { signup, login, logout } from './actions/session_actions';
 import configureStore from './store/store';
 import Root from './components/root';
 
@@ -12,9 +12,9 @@ document.addEventListener('DOMContentLoaded', () => {
 	window.getState = store.getState;
 	window.dispatch = store.dispatch;
 
-	window.signup = signup;
-	window.login = login;
-	window.logout = logout;
+	// window.signup = signup;
+	// window.login = login;
+	// window.logout = logout;
 	// Test end
 
 	ReactDOM.render(<Root store={store} />, root);
