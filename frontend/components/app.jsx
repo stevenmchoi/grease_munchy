3e85002fa794ece3b79e0d21811d4dc445c7cbf1
Steven Choi <steven.m.choi@gmail.com>
2018-04-06 02:29:44 -0700

    Create Login form; same as Signup form

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import NavBarContainer from './nav_bar/nav_bar_container';
 import SignupContainer from './signup/signup_container';
+import LoginContainer from './login/login_container';
 
 const App = () => (
 	<div>
 		<Route path="/" component={NavBarContainer} />
 		<Route path="/signup" component={SignupContainer} />
+		<Route path="/login" component={LoginContainer} />
 	</div>
 );
 
---
+++ frontend/components/login/login.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+class Login extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: '',
+			password: '',
+		};
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(field) {
+		return (e) => {
+			this.setState({ [field]: e.target.value });
+		};
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		this.props.login(this.state).then(() => this.props.history.push('/'));
+	}
+
+	render() {
+		const { username, password } = this.state;
+
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<label>
+					Username:
+					<input
+						type="text"
+						value={`${username}`}
+						onChange={this.handleChange('username')}
+					/>
+				</label>
+
+				<label>
+					Password:
+					<input
+						type="password"
+						value={`${password}`}
+						onChange={this.handleChange('password')}
+					/>
+				</label>
+
+				<button type="submit">Log in</button>
+			</form>
+		);
+	}
+}
+
+export default Login;
