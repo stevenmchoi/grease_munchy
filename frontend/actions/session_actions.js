84a3cc1295f7fea976b65636ba8af423e8a34e7b
Steven Choi <steven.m.choi@gmail.com>
2018-04-05 17:54:02 -0700

    Fix state shape for errors to use responseJSON, since .then return entire promise for failure functions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -6,28 +6,28 @@ export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 const receiveCurrentUser = (currentUser) => ({
 	type: RECEIVE_CURRENT_USER,
-	currentUser
+	currentUser,
 });
 
 const receiveErrors = (errors) => ({
 	type: RECEIVE_SESSION_ERRORS,
-	errors
+	errors,
 });
 
 export const signup = (newUser) => (dispatch) =>
 	SessionApiUtil.signup(newUser).then(
 		(user) => dispatch(receiveCurrentUser(user)),
-		(errors) => dispatch(receiveErrors(errors))
+		(errors) => dispatch(receiveErrors(errors.responseJSON))
 	);
 
 export const login = (existingUser) => (dispatch) =>
 	SessionApiUtil.login(existingUser).then(
 		(user) => dispatch(receiveCurrentUser(user)),
-		(errors) => dispatch(receiveErrors(errors))
+		(errors) => dispatch(receiveErrors(errors.responseJSON))
 	);
 
 export const logout = () => (dispatch) =>
 	SessionApiUtil.logout().then(
 		(user) => dispatch(receiveCurrentUser(null)),
-		(errors) => dispatch(receiveErrors(errors))
+		(errors) => dispatch(receiveErrors(errors.responseJSON))
 	);
