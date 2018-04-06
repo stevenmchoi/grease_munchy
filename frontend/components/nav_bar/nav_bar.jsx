9779b491f790d7fe1ea56c3a53886067988ea1db
Steven Choi <steven.m.choi@gmail.com>
2018-04-05 17:30:17 -0700

    Add session buttons to NavBar

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-class NavBar extends React.Component {
-	render() {
-		return (
-			<div>
-				<a href="/">
-					<img
-						src="https://fontmeme.com/permalink/180401/ebd8cc51ec8f24ef053f3daebd6b9506.png"
-						alt="blue-apron-logo-font"
-						border="0"
-					/>
-				</a>
-				{/* <!-- Logo from Font Meme:
+const NavBar = ({ currentUser, logout }) => {
+	const sessionButtons = currentUser ? (
+		<span>
+			<p>{currentUser.username}</p>
+			<button type="submit" onClick={logout}>
+				Log Out
+			</button>
+		</span>
+	) : (
+		<span>
+			<Link to="/login">Log In</Link>
+			<Link to="/signup">Sign Up</Link>
+		</span>
+	);
+	return (
+		<header>
+			<a href="/">
+				<img
+					src="https://fontmeme.com/permalink/180401/ebd8cc51ec8f24ef053f3daebd6b9506.png"
+					alt="blue-apron-logo-font"
+					border="0"
+				/>
+			</a>
+			{/* <!-- Logo from Font Meme:
 			https://fontmeme.com/blue-apron-logo-font/ --> */}
-				<h1>Grease Munchy...why not?</h1>
-			</div>
-		);
-	}
-}
+
+			<h1>Grease Munchy...why not?</h1>
+
+			{sessionButtons}
+		</header>
+	);
+};
 
 export default NavBar;
