7383ff1b97b588545e7c770e97402a19a0089dc6
Steven Choi <steven.m.choi@gmail.com>
2018-04-06 10:38:48 -0700

    Plan out other components

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -7,8 +7,25 @@ import LoginContainer from './login/login_container';
 const App = () => (
 	<div>
 		<Route path="/" component={NavBarContainer} />
+
+		{/* <MainContainer> */}
+		{/* <Switch> */}
 		<Route path="/signup" component={SignupContainer} />
 		<Route path="/login" component={LoginContainer} />
+
+		{/* <Route exact path='/' component={SplashContainer} /> // logged out */}
+		{/* <Route exact path='/account' component={SplashContainer} /> // logged in */}
+
+		{/* <Route path="/pages/sample-recipes" component={MenuContainer} /> */}
+		{/* <Route path="/account#upcoming" component={MenuContainer} /> */}
+
+		{/* <Route path="/pages/pricing#plan-selector" component={MealPlanContainer} /> */}
+		{/* <Route path="/pages/pricing" component={MealPlanContainer} /> */}
+
+		{/* <Switch> */}
+		{/* </ MainContainer> */}
+
+		{/* <Route path='/' component={FooterContainer} /> */}
 	</div>
 );
 
