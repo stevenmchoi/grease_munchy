615752ab8606a6036d97ff14f4e65fddaabe8b0b
Steven Choi <steven.m.choi@gmail.com>
2018-04-08 00:20:36 -0700

    Refactor app.jsx and main.jsx to have main contain the login forms

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { AuthRoute } from '../util/route_util';
 import NavBarContainer from './nav_bar/nav_bar_container';
-import SignupContainer from './signup/signup_container';
-import LoginContainer from './login/login_container';
 // import MainContainer from './main/main_container';
 import Main from './main/main';
 
 const App = () => (
-	<div>
+	<div className="app-container">
 		<Route path="/" component={NavBarContainer} />
 
-		<Switch>
+		<Route component={Main} />
+
+		{/* <Route path='/' component={FooterContainer} /> */}
+
+		{/* <Switch>
 			<AuthRoute path="/signup" component={SignupContainer} />
 			<AuthRoute path="/login" component={LoginContainer} />
 			<Route component={Main} />
-		</Switch>
-
-		{/* <Route path='/' component={FooterContainer} /> */}
+		</Switch> */}
 	</div>
 );
 
--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../../util/route_util';
+
+import SignupContainer from '../signup/signup_container';
+import LoginContainer from '../login/login_container';
 import AccountContainer from './account/account_container';
 
 const Main = () => {
 	return (
-		<main>
+		<section className="main-section">
+			<div className="spacer-behind-nav" />
+
 			<h1>Welcome to Grease Munchy!</h1>
+
 			<Switch>
+				<AuthRoute path="/signup" component={SignupContainer} />
+				<AuthRoute path="/login" component={LoginContainer} />
+
 				{/* <Route
 					path="/pages/sample-recipes"
 					component={MenuContainer}
@@ -36,9 +45,26 @@ const Main = () => {
 				/> */}
 
 				<ProtectedRoute path="/account" component={AccountContainer} />
-				{/* <Route component={SplashContainer} /> */}
+
+				{/* <Route component={SplashContainer} />  // default container */}
 			</Switch>
-		</main>
+
+			<p>
+				owiejf owiejfweofjo<br />
+				woiejfowiejfoiwejfwoei woiejfoiwejfoniwnofiej
+				<br />owiej
+			</p>
+			<p>oiwjeoifjowiejf owjeof iwjeo fwoe fowiejfowje</p>
+			<p>iwewoeijfo iowijef orijfowiejfowiefoifo</p>
+			<p>soidufoiwjfowjefjwo owie</p>
+			<p>
+				foiej woiefo we fowieofiw eoifowe fiw efi woiejfowiejfoiwejfwoei
+				oi
+			</p>
+			<p>jiw fowj oefjoie f jbfhjwlkej,</p>
+			<p>wjrjgne,rjg n, rg oefijwoiehiuwhy48hi384y3984yo</p>
+			<p>34o8 t3o 4to387 4 34to83 4</p>
+		</section>
 	);
 };
 
