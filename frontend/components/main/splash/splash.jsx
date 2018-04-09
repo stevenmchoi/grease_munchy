6b8c8f0b37b55e407c7e1e253cce827d86284b7b
Steven Choi <steven.m.choi@gmail.com>
2018-04-09 15:11:36 -0700

    Add Splash video to root page

--- frontend/components/main/splash/splash.jsx
+++ frontend/components/main/splash/splash.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const Splash = () => <div className="content-header" />;
+const Splash = () => (
+	<div className="content-header">
+		<video preload="auto" autoPlay loop>
+			<source
+				src="https://www.videvo.net/videvo_files/converted/2013_11/preview/Clip4271013Videvo.mov90234.webm"
+				type="video/webm"
+			/>
+		</video>
+	</div>
+);
 
 export default Splash;
