ec8448a6e497033faf15023693afa2bd3f7e288b
Steven Choi <steven.m.choi@gmail.com>
2018-04-09 11:14:57 -0700

    Make unique keys for nav-button links

--- frontend/components/nav_bar/nav_buttons/nav_buttons.jsx
+++ frontend/components/nav_bar/nav_buttons/nav_buttons.jsx
@@ -4,13 +4,23 @@ import { Link } from 'react-router-dom';
 const NavButtons = ({ currentUser }) => {
 	return currentUser
 		? [
-				<Link to="/account#upcoming">Upcoming</Link>,
-				<Link to="/account#create">Create Meal Plan</Link>,
-				<Link to="/recipes">Cookbook</Link>,
+				<Link key="upcoming" to="/account#upcoming">
+					Upcoming
+				</Link>,
+				<Link key="create" to="/account#create">
+					Create Meal Plan
+				</Link>,
+				<Link key="recipes" to="/recipes">
+					Cookbook
+				</Link>,
 		  ]
 		: [
-				<Link to="/menu">On The Menu</Link>,
-				<Link to="/recipes">Cookbook</Link>,
+				<Link key="menu" to="/menu">
+					On The Menu
+				</Link>,
+				<Link key="recipes" to="/recipes">
+					Cookbook
+				</Link>,
 		  ];
 };
 
