8244ccd9382ebfc12d080505ee7ee5e4d57780a2
Steven Choi <steven.m.choi@gmail.com>
2018-04-09 00:14:55 -0700

    Make logo sharper as text

--- frontend/components/nav_bar/logo.jsx
+++ frontend/components/nav_bar/logo.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 const Logo = () => (
-	<img
-		className="logo"
-		src="https://fontmeme.com/permalink/180401/ebd8cc51ec8f24ef053f3daebd6b9506.png"
-		alt="blue-apron-logo-font"
-		border="0"
-	/> // Logo from Font Meme: https://fontmeme.com/blue-apron-logo-font/
+	<div className="logo-wrapper">
+		<h1 className="logo">Grease Munchy</h1>
+	</div>
+	// <img
+	// 	className="logo"
+	// 	src="https://fontmeme.com/permalink/180401/ebd8cc51ec8f24ef053f3daebd6b9506.png"
+	// 	alt="blue-apron-logo-font"
+	// 	border="0"
+	// /> // Logo from Font Meme: https://fontmeme.com/blue-apron-logo-font/
 );
 
 export default Logo;
