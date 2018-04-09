bab453d4a69ec58472f01f7bab235937ca3bf620
Steven Choi <steven.m.choi@gmail.com>
2018-04-09 15:27:34 -0700

    Get rid of unnecessary files

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import NavBarContainer from './nav_bar/nav_bar_container';
-// import MainContainer from './main/main_container';
 import Main from './main/main';
 
 const App = () => (
--- frontend/components/main/main_container.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Main from './main';
-
-const mapStateToProps = ({ session: { currentUser } }) => ({
-	currentUser,
-});
-
-export default connect(mapStateToProps)(Main);
