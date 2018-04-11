12e7ef3a96bd5fd84c51aa7aade3ed5feb20df58
Steven Choi <steven.m.choi@gmail.com>
2018-04-10 17:28:39 -0700

    Seed db with Meal items

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,43 @@
 # 404: Not Found: route doesn't exist
 # 422: Unprocessable Entity: Bad fields for signup
 
-User.create([{
-	username: 'demo',
-	email: 'demo@demo',
-	password: 'password'
-}])
+User.create([{ username: 'demo', email: 'demo@demo', password: 'password' }])
+
+Meal.create([
+	{
+		name: "Big Mac",
+		description: "Tasty!",
+		imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_02_Videvo.mov63142.webm",
+		restaurant: "McDonald's"
+	},
+	{
+		name: "Large Fries",
+		description: "Nowhere else!",
+		imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_03_Videvo.mov69703.webm",
+		restaurant: "McDonald's"
+	},
+	{
+		name: "McFlurry",
+		description: "Jason Wu!",
+		imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_02_Videvo.mov63142.webm",
+		restaurant: "McDonald's"
+	},
+	{
+		name: "Dave's Double",
+		description: "I'm getting hungry now...but we're in SF",
+		imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_03_Videvo.mov69703.webm",
+		restaurant: "Wendy's"
+	},
+	{
+		name: "Two-Piece Combo",
+		description: "Someone add Popeyes!",
+		imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_02_Videvo.mov63142.webm",
+		restaurant: "KFC"
+	},
+	{
+		name: "Classic Buttery Jack",
+		description: "How many times did I eat this at 12am?",
+		imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_03_Videvo.mov69703.webm",
+		restaurant: "Jack In The Box"
+	},
+])
