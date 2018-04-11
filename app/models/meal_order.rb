edd323102c343973f622ba56ba37f050e3298f66
Steven Choi <steven.m.choi@gmail.com>
2018-04-10 23:42:14 -0700

    Create MealOrders and more test Users

--- app/models/meal_order.rb
+++ app/models/meal_order.rb
@@ -7,7 +7,7 @@ class MealOrder < ApplicationRecord
 		foreign_key: :user_id,
 		primary_key: :id
 
-	belongs_to :meal
+	belongs_to :meal,
 		class_name: :Meal,
 		foreign_key: :meal_id,
 		primary_key: :id
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,43 +14,54 @@
 # 404: Not Found: route doesn't exist
 # 422: Unprocessable Entity: Bad fields for signup
 
+User.create([{ username: 'user1', email: 'user1@user1', password: 'password' }])
+User.create([{ username: 'user2', email: 'user2@user2', password: 'password' }])
 User.create([{ username: 'demo', email: 'demo@demo', password: 'password' }])
 
-Meal.create([
-	{
-		name: "Big Mac",
-		description: "Tasty!",
-		imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_02_Videvo.mov63142.webm",
-		restaurant: "McDonald's"
-	},
-	{
-		name: "Large Fries",
-		description: "Nowhere else!",
-		imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_03_Videvo.mov69703.webm",
-		restaurant: "McDonald's"
-	},
-	{
-		name: "McFlurry",
-		description: "Jason Wu!",
-		imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_02_Videvo.mov63142.webm",
-		restaurant: "McDonald's"
-	},
-	{
-		name: "Dave's Double",
-		description: "I'm getting hungry now...but we're in SF",
-		imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_03_Videvo.mov69703.webm",
-		restaurant: "Wendy's"
-	},
-	{
-		name: "Two-Piece Combo",
-		description: "Someone add Popeyes!",
-		imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_02_Videvo.mov63142.webm",
-		restaurant: "KFC"
-	},
-	{
-		name: "Classic Buttery Jack",
-		description: "How many times did I eat this at 12am?",
-		imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_03_Videvo.mov69703.webm",
-		restaurant: "Jack In The Box"
-	},
-])
+Meal.create([{
+	name: "Big Mac",
+	description: "Tasty!",
+	imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_02_Videvo.mov63142.webm",
+	restaurant: "McDonald's"
+}])
+Meal.create([{
+	name: "Large Fries",
+	description: "Nowhere else!",
+	imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_03_Videvo.mov69703.webm",
+	restaurant: "McDonald's"
+}])
+Meal.create([{
+	name: "McFlurry",
+	description: "Jason Wu!",
+	imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_02_Videvo.mov63142.webm",
+	restaurant: "McDonald's"
+}])
+Meal.create([{
+	name: "Dave's Double",
+	description: "I'm getting hungry now...but we're in SF",
+	imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_03_Videvo.mov69703.webm",
+	restaurant: "Wendy's"
+}])
+Meal.create([{
+	name: "Two-Piece Combo",
+	description: "Someone add Popeyes!",
+	imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_02_Videvo.mov63142.webm",
+	restaurant: "KFC"
+}])
+Meal.create([{
+	name: "Classic Buttery Jack",
+	description: "How many times did I eat this at 12am?",
+	imageUrl: "https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_03_Videvo.mov69703.webm",
+	restaurant: "Jack In The Box"
+}])
+
+MealOrder.create([{ user_id: 1, meal_id: 1, date: "2018-04-14" }])
+MealOrder.create([{ user_id: 1, meal_id: 6, date: "2018-04-14" }])
+MealOrder.create([{ user_id: 1, meal_id: 1, date: "2018-04-21" }])
+MealOrder.create([{ user_id: 2, meal_id: 1, date: "2018-04-12" }])
+MealOrder.create([{ user_id: 2, meal_id: 5, date: "2018-04-12" }])
+MealOrder.create([{ user_id: 2, meal_id: 1, date: "2018-04-14" }])
+MealOrder.create([{ user_id: 2, meal_id: 5, date: "2018-04-14" }])
+MealOrder.create([{ user_id: 2, meal_id: 2, date: "2018-04-19" }])
+MealOrder.create([{ user_id: 2, meal_id: 1, date: "2018-04-19" }])
+MealOrder.create([{ user_id: 2, meal_id: 1, date: "2018-04-26" }])
