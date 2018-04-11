90034f36fdcc3ca0697edbe43f5f29d4f0d062b6
Steven Choi <steven.m.choi@gmail.com>
2018-04-11 09:50:41 -0700

    Fill out mealOrderReducer for now

---
+++ frontend/reducers/entities_reducer/meal_order_reducer.js
@@ -0,0 +1,23 @@
+import {
+	RECEIVE_ALL_MEAL_ORDERS,
+	RECEIVE_MEAL_ORDER,
+} from '../../actions/meal_orders_actions';
+import merge from 'lodash/merge';
+
+const mealOrderReducer = (oldState = {}, action) => {
+	Object.freeze(oldState);
+	let newState = merge({}, oldState);
+
+	switch (action.type) {
+		case RECEIVE_ALL_MEAL_ORDERS:
+			newState.mealOrders = action.mealOrders;
+			// Should already be filtered out in 'view/index.json.jbuilder'
+			return newState;
+		case RECEIVE_MEAL_ORDER:
+			return action.mealOrder;
+		default:
+			return oldState;
+	}
+};
+
+export default mealOrderReducer;
