c87f9fb33ca655064cb0f2675b4f01284c6e4abf
Steven Choi <steven.m.choi@gmail.com>
2018-04-11 12:12:45 -0700

    Add optional error handling for the future

--- app/controllers/api/meals_controller.rb
+++ app/controllers/api/meals_controller.rb
@@ -1,15 +1,24 @@
 class Api::MealsController < ApplicationController
 	def index
 		@meals = Meal.all
+
+		# render json: @meals
 	end
 
 	def show
-		@meal = Meal.find(meals_params)
+		@meal = Meal.find(params[:id])
+
+		# if @meal
+		# 	render :show
+		# else
+		# 	puts "Hwoieoiw-e0---------wo-03--0-0i-0i-203i-02i-302-3i0"
+		# 	render json: ["Can't find meal!"], status: 404
+		# end
 	end
 
 	private
 
-	def meals_params
+	def meals_params # for search fn later
 		params.require(:meal).permit(:name, :restaurant)
 	end
 end
