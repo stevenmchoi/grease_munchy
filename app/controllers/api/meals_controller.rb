78d74914b7bbb3cdf6c716ec3f64f02083204028
Steven Choi <steven.m.choi@gmail.com>
2018-04-11 05:33:08 -0700

    Create controllers/routes for Meals and MealOrders

---
+++ app/controllers/api/meals_controller.rb
@@ -0,0 +1,9 @@
+class Api::MealsController < ApplicationController
+	def index
+
+	end
+
+	def show
+
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
+
+    resources :meal_orders, only: [:index, :create, :destroy]
+    resources :meals, only: [:index, :show]
   end
 end
