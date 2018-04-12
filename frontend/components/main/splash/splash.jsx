fa04b6ac0d1d5d52d20764141a49e2fe6b52d658
Steven Choi <steven.m.choi@gmail.com>
2018-04-11 18:01:13 -0700

    Link splash page video to /signup

--- frontend/components/main/splash/splash.jsx
+++ frontend/components/main/splash/splash.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Splash = () => (
 	<div className="content-header">
 		<h1>Your house is the drive-thru</h1>
-		<video
-			preload="auto"
-			autoPlay
-			loop
-			src="https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_03_Videvo.mov69703.webm"
-			type="video/webm"
-		/>
+		<Link to="/signup">
+			<video
+				preload="auto"
+				autoPlay
+				loop
+				src="https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_03_Videvo.mov69703.webm"
+				type="video/webm"
+			/>
+		</Link>
 	</div>
 );
 
