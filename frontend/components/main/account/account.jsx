6222d102d73910cf357602202c41ca839cc904f4
Steven Choi <steven.m.choi@gmail.com>
2018-04-12 03:39:28 -0700

    Fix up home page and nav bar

--- frontend/components/main/account/account.jsx
+++ frontend/components/main/account/account.jsx
@@ -3,10 +3,10 @@ import { Switch, Route } from 'react-router-dom';
 
 const AccountMain = ({ currentUser }) => {
 	return (
-		<section className="content-header">
+		<div className="content-header">
 			<h1>Get your fast food faster, {currentUser.username}!</h1>
 			<img src="https://farm3.staticflickr.com/2572/3932344564_c03b7326c1_o.jpg" />
-		</section>
+		</div>
 	);
 };
 
