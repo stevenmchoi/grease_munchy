9764663eab06df2e7068a338793c056a2bb16a7b
Steven Choi <steven.m.choi@gmail.com>
2018-04-12 15:29:08 -0700

    Finish putting all elements on RecipeShow page

--- frontend/components/main/recipes/recipe_show/recipe_show.jsx
+++ frontend/components/main/recipes/recipe_show/recipe_show.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// TODO: Combine RecipeModal with RecipeShow
-class RecipeShow extends React.Component {}
+const RecipeShow = ({ meal, mealId, fetchMeal }) => {
+	if (!meal || !meal.servings) {
+		fetchMeal(mealId);
+		return null;
+	} else {
+		return (
+			<div className="recipe-show">
+				<ul className="recipe">
+					<h3>{meal.name}</h3>
+					<li>Description: {meal.description}</li>
+					<img className="recipe-img" src={meal.imageUrl} />
+					<li>Restaurant: {meal.restaurant}</li>
+					<li>Ingredients: {meal.ingredients}</li>
+					<li>Instructions: {meal.instructions}</li>
+					<li>Servings: {meal.servings}</li>
+				</ul>
+
+				<Link to="/recipes">{`< Back`}</Link>
+			</div>
+		);
+	}
+};
 
 export default RecipeShow;
--- frontend/components/main/recipes/recipe_show/recipe_show_container.jsx
+++ frontend/components/main/recipes/recipe_show/recipe_show_container.jsx
@@ -2,10 +2,13 @@ import { connect } from 'react-redux';
 import RecipeShow from './recipe_show';
 import { fetchMeal } from '../../../../actions/meals_actions';
 
-const mapStateToProps = ({ entities: { meals } }) => ({
-	meals,
-	// meal: meals[] // TODO: see if I can get the meal here
-});
+const mapStateToProps = ({ entities: { meals } }, ownProps) => {
+	const mealId = ownProps.match.params.recipeNameId.match(/\d+/)[0];
+	return {
+		meal: meals[mealId],
+		mealId,
+	};
+};
 
 const mapDispatchToProps = (dispatch) => ({
 	fetchMeal: (mealId) => dispatch(fetchMeal(mealId)),
