0795a9995a873e9f64acbcd530f3ba2ee2df2502
Steven Choi <steven.m.choi@gmail.com>
2018-04-13 11:27:41 -0700

    Get app prod ready

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,49 @@
-const path = require("path");
+const path = require('path');
+const webpack = require('webpack');
 
+let plugins = []; // if using any plugins for both dev and production
+let devPlugins = []; // if using any plugins for development
+
+let prodPlugins = [
+	new webpack.DefinePlugin({
+		'process.env': {
+			NODE_ENV: JSON.stringify('production'),
+		},
+	}),
+	new webpack.optimize.UglifyJsPlugin({
+		compress: {
+			warnings: true,
+		},
+	}),
+];
+
+plugins = plugins.concat(
+	process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
+// include plugins config
 module.exports = {
-  context: __dirname,
-  entry: "./frontend/entry.jsx",
-  output: {
-    path: path.resolve(__dirname, "app", "assets", "javascripts"),
-    filename: "bundle.js"
-  },
-  module: {
-    loaders: [
-      {
-        test: [/\.jsx?$/, /\.js?$/],
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
-          presets: ["env", "react"]
-        }
-      }
-    ]
-  },
-  devtool: "source-map",
-  resolve: {
-    extensions: [".js", ".jsx", "*"]
-  }
+	context: __dirname,
+	entry: './frontend/entry.jsx',
+	output: {
+		path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+		filename: 'bundle.js',
+	},
+	plugins: plugins,
+	module: {
+		loaders: [
+			{
+				test: [/\.jsx?$/, /\.js?$/],
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['env', 'react'],
+				},
+			},
+		],
+	},
+	devtool: 'source-map',
+	resolve: {
+		extensions: ['.js', '.jsx', '*'],
+	},
 };
