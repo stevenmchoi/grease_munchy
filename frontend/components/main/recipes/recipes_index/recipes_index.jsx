8edc5dbda9c43a36b750f977e8536faba89369f2
Steven Choi <steven.m.choi@gmail.com>
2018-04-12 17:28:16 -0700

    Delete commented code

--- frontend/components/main/recipes/recipes_index/recipes_index.jsx
+++ frontend/components/main/recipes/recipes_index/recipes_index.jsx
@@ -3,7 +3,6 @@ import Slide from 'react-reveal/Slide';
 import Fade from 'react-reveal/Fade';
 import RecipeModal from './recipe_modal';
 
-// TODO: Doesn't refresh when reached from meal show page
 class RecipesIndex extends React.Component {
 	constructor(props) {
 		super(props);
@@ -29,25 +28,5 @@ class RecipesIndex extends React.Component {
 		);
 	}
 }
-
-// const RecipesIndex = ({ meals, ownProps, fetchAllMeals }) => {
-// 	console.log(ownProps);
-
-// 	if (meals.length == 0) {
-// 		fetchAllMeals();
-// 	}
-
-// 	return (
-// 		<Fade bottom cascade>
-// 			<ul className="recipes-list">
-// 				{meals.map((meal) => (
-// 					<li key={`recipe-${meal.id}`}>
-// 						<RecipeModal meal={meal} id={meal.id} />
-// 					</li>
-// 				))}
-// 			</ul>
-// 		</Fade>
-// 	);
-// };
 
 export default RecipesIndex;
