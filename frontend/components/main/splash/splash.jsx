e0e5cef7ad42f1e9888568c4c8fb6a2119c364fd
Steven Choi <steven.m.choi@gmail.com>
2018-05-28 22:05:33 -0700

    Add muted property so that Chrome 66 can autoplay React video element

--- frontend/components/main/splash/splash.jsx
+++ frontend/components/main/splash/splash.jsx
@@ -12,6 +12,7 @@ const Splash = () => (
 				loop
 				src="https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_03_Videvo.mov69703.webm"
 				type="video/webm"
+				muted
 			/>
 		</Link>
 	</div>
