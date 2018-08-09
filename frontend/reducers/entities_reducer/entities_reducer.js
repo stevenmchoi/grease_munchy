233c87fbc82d95749ed6b3666c426c2f55282697
Steven Choi <steven.m.choi@gmail.com>
2018-08-09 13:06:47 -0700

    Bring menu items into entities reducer

--- frontend/reducers/entities_reducer/entities_reducer.js
+++ frontend/reducers/entities_reducer/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import mealsReducer from "./meals_reducer";
 import mealOrdersReducer from "./meal_orders_reducer";
+import menuItemsReducer from "./menu_items_reducer";
 
 const entitiesReducer = combineReducers({
 	meals: mealsReducer,
 	mealOrders: mealOrdersReducer,
+	menuItems: menuItemsReducer
 });
 
 export default entitiesReducer;
