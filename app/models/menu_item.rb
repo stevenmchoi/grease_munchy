f03174692ba20d051ed87f3e53c01e5afdbc4753
Steven Choi <steven.m.choi@gmail.com>
2018-08-09 08:58:23 -0700

    Create MenuItem model

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,3 +1,9 @@
 class MenuItem < ApplicationRecord
-    
+    validates :date, :meal_id, presence: true
+	validates_uniqueness_of :date, scope: :meal_id
+
+	belongs_to :meal,
+		class_name: :Meal,
+		foreign_key: :meal_id,
+		primary_key: :id
 end
