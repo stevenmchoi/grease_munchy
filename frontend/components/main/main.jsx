625b0b455212b07cab1a85fad57e3613dad3afb5
Steven Choi <steven.m.choi@gmail.com>
2018-08-10 01:44:35 -0700

    Add Menu Container to Main component

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -4,13 +4,14 @@ import { AuthRoute, ProtectedRoute } from "../../util/route_util";
 
 import SignupContainer from "./signup/signup_container";
 import LoginContainer from "./login/login_container";
-import SplashContainer from "./splash/splash_container";
+
+import MenuContainer from "./menu/menu_index_container";
+
 import RecipesIndexContainer from "./recipes/recipes_index/recipes_index_container";
 import RecipeShowContainer from "./recipes/recipe_show/recipe_show_container";
 
-// import ManageMealPlanContainer from "./account/manage_meal_plan/manage_meal_plan_container";
-// import CreateMealPlanContainer from "./account/create_meal_plan/create_meal_plan_container";
 import AccountContainer from "./account/account_container";
+import SplashContainer from "./splash/splash_container";
 
 const Main = () => {
   return (
@@ -21,20 +22,13 @@ const Main = () => {
         <AuthRoute path="/signup" component={SignupContainer} />
         <AuthRoute path="/login" component={LoginContainer} />
 
-        {/* <Route
-					path="/account/upcoming"
-					component={MenuContainer}
-				/> */}
+        <ProtectedRoute path="/account#upcoming" component={MenuContainer} />
+        <AuthRoute path="/menu" component={MenuContainer} />
 
         <Route path="/recipes/:recipeNameId" component={RecipeShowContainer} />
         <Route path="/recipes" component={RecipesIndexContainer} />
 
-        {/* <ProtectedRoute
-					path="/account/mealplan"
-					component={CreateMealPlanContainer}
-				/> */}
         <ProtectedRoute path="/account" component={AccountContainer} />
-
         <AuthRoute path="/" component={SplashContainer} />
       </Switch>
     </div>
--- frontend/components/nav_bar/nav_buttons/nav_buttons.jsx
+++ frontend/components/nav_bar/nav_buttons/nav_buttons.jsx
@@ -2,26 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NavButtons = ({ currentUser }) => {
-	return currentUser
-		? [
-				<Link key="upcoming" to="/account#upcoming">
-					Upcoming
-				</Link>,
-				<Link key="create" to="/account/mealplan">
-					Create Meal Plan
-				</Link>,
-				<Link key="recipes" to="/recipes">
-					Recipes
-				</Link>,
-		  ]
-		: [
-				<Link key="menu" to="/menu">
-					On The Menu
-				</Link>,
-				<Link key="recipes" to="/recipes">
-					Recipes
-				</Link>,
-		  ];
+  return currentUser
+    ? [
+        <Link key="upcoming" to="/account#upcoming">
+          Upcoming
+        </Link>,
+        <Link key="recipes" to="/recipes">
+          Recipes
+        </Link>
+      ]
+    : [
+        <Link key="menu" to="/menu">
+          On The Menu
+        </Link>,
+        <Link key="recipes" to="/recipes">
+          Recipes
+        </Link>
+      ];
 };
 
 export default NavButtons;
