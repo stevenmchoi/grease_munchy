d58cc47f2627b7a48a8beb477c7d347da0ece6a2
Steven Choi <steven.m.choi@gmail.com>
2018-10-18 05:57:01 -0700

    Grab MenuIndex vars from props

--- frontend/components/main/menu/menu_index.jsx
+++ frontend/components/main/menu/menu_index.jsx
@@ -3,65 +3,47 @@ import Fade from 'react-reveal/Fade';
 import MenuItemModal from './menu_item_modal';
 
 class MenuIndex extends Component {
-	constructor(props) {
-		super(props);
-
-		this.currentUser = props.currentUser;
-		this.meals = props.meals;
-		this.menuItems = props.menuItems;
-		this.numMenuItems = props.numMenuItems;
-		this.fetchMeal = props.fetchMeal;
-		this.fetchAllMenuItems = props.fetchAllMenuItems;
-		this.fetchMealOrders = props.fetchMealOrders;
-		this.createMealOrder = props.createMealOrder;
-		this.deleteMealOrder = props.deleteMealOrder;
+	componentDidMount() {
+		this.props.fetchAllMenuItems();
 	}
 
 	render() {
-		if (menuItems.length <= numMenuItems) {
-			fetchAllMenuItems();
-			return null;
-		} else {
+		return this.props.menuItems.map((menuItemsByWeek) => {
+			let menuWeekItems = Object.values(menuItemsByWeek);
+			let numMenuWeek = menuWeekItems.length;
+			let weekOf = menuWeekItems[0].date;
+
 			return (
-				<Fade right cascade>
-					{menuItems.map((menuItemsByWeek) => {
-						let menuWeekItems = Object.values(menuItemsByWeek);
-						let numMenuWeek = menuWeekItems.length;
-						let weekOf = menuWeekItems[0].date;
+				<ul className="menu-week" key={`week-${weekOf}`}>
+					<h1>Week of {weekOf}</h1>
 
-						return (
-							<ul className="menu-week" key={`week-${weekOf}`}>
-								<h1>Week of {weekOf}</h1>
+					<ul className="menu-list" key={`menu-on-${weekOf}`}>
+						{menuWeekItems.map((menuItem) => {
+							const meal = this.props.meals[menuItem.meal_id];
 
-								<ul className="menu-list" key={`menu-on-${weekOf}`}>
-									{menuWeekItems.map((menuItem) => {
-										const meal = meals[menuItem.meal_id];
+							if (!this.props.meals[menuItem.meal_id]) {
+								this.props.fetchMeal(menuItem.meal_id);
 
-										if (!meals[menuItem.meal_id]) {
-											fetchMeal(menuItem.meal_id);
-											return null;
-										} else {
-											return (
-												<li key={`menu-item-${menuItem.meal_id}`}>
-													<MenuItemModal
-														fetchMealOrders={this.fetchMealOrders}
-														createMealOrder={this.createMealOrder}
-														deleteMealOrder={this.deleteMealOrder}
-														currentUser={this.currentUser}
-														meal={this.meal}
-														menuItem={this.menuItem}
-													/>
-												</li>
-											);
-										}
-									})}
-								</ul>
-							</ul>
-						);
-					})}
-				</Fade>
+								return null;
+							} else {
+								return (
+									<li key={`menu-item-${menuItem.meal_id}`}>
+										<MenuItemModal
+											fetchMealOrders={this.props.fetchMealOrders}
+											createMealOrder={this.props.createMealOrder}
+											deleteMealOrder={this.props.deleteMealOrder}
+											currentUser={this.props.currentUser}
+											meal={this.props.meal}
+											menuItem={this.props.menuItem}
+										/>
+									</li>
+								);
+							}
+						})}
+					</ul>
+				</ul>
 			);
-		}
+		});
 	}
 }
 
