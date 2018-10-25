a6d914aebdee298f24b272a56ccea0d96984b211
Steven Choi <steven.m.choi@gmail.com>
2018-10-25 14:09:08 -0700

    Remove scheduling rake tasks

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,12 +18,12 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
-set :output, '/log/cron_log.log'
+# set :output, '/log/cron_log.log'
 
 # every :sunday, at: '12am' do
 #   rake 'menu:create_menu_items'
 # end
 
-every 1.minute do
-  rake 'menu:create_menu_items'
-end
+# every 1.minute do
+#   rake 'menu:create_menu_items'
+# end
