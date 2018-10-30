74bc4a3dd1a3a3edd26ea83ec7a84fef08beafe9
Steven Choi <steven.m.choi@gmail.com>
2018-10-30 02:41:00 -0700

    Rearrange image and text

--- frontend/components/main/account/account.jsx
+++ frontend/components/main/account/account.jsx
@@ -3,7 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 const AccountMain = ({ currentUser }) => {
 	return (
-		<div className="content-header">
+		<div className="splash-header">
 			<h1>Get your fast food faster, {currentUser.username}!</h1>
 			<img src="https://farm3.staticflickr.com/2572/3932344564_c03b7326c1_o.jpg" />
 		</div>
--- frontend/components/main/splash/splash.jsx
+++ frontend/components/main/splash/splash.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Splash = () => (
-	<div className="content-header">
-		<h1>Your house is the drive-thru</h1>
-		<Link to="/signup">
-			<video
-				className="home-video"
-				preload="auto"
-				autoPlay
-				loop
-				src="https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_03_Videvo.mov69703.webm"
-				type="video/webm"
-				muted
-			/>
-		</Link>
+	<div className="splash-header">
+		<video
+			className="home-video"
+			preload="auto"
+			autoPlay
+			loop
+			src="https://www.videvo.net/videvo_files/converted/2015_04/preview/FoodPack1_03_Videvo.mov69703.webm"
+			type="video/webm"
+			muted
+		/>
+		<div className="splash-header-container">
+			<h2 className="splash-header-text">Your house is the drive-thru!</h2>
+		</div>
 	</div>
 );
 
