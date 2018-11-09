f68abae06f3deb560c2045ec0618fb2166cd27f9
Steven Choi <steven.m.choi@gmail.com>
2018-11-09 03:12:44 -0800

    Debug missing var

--- webpack.config.js
+++ webpack.config.js
@@ -5,26 +5,24 @@ const webpack = require('webpack');
 let plugins = []; // if using any plugins for both dev and production
 let devPlugins = []; // if using any plugins for development
 
-// let prodPlugins = [
-// new webpack.DefinePlugin({
-// 	'process.env': {
-// 		NODE_ENV: JSON.stringify('production'),
-// 	},
-// }),
-// new webpack.optimize.UglifyJsPlugin({
-// 	compress: {
-// 		warnings: true,
-// 	},
-// }),
-
-// new UglifyJsPlugin(), // Replaced the reference to webpack.optimize.UglifyJsPlugin
-
-// new CompressionPlugin({
-// 	asset: '[path].gz[query]',
-// 	algorithm: 'gzip',
-// 	test: /\.(js|css|html|json|ico|svg|eot|otf|ttf)$/,
-// }),
-// ];
+let prodPlugins = [
+	// new webpack.DefinePlugin({
+	// 	'process.env': {
+	// 		NODE_ENV: JSON.stringify('production'),
+	// 	},
+	// }),
+	// new webpack.optimize.UglifyJsPlugin({
+	// 	compress: {
+	// 		warnings: true,
+	// 	},
+	// }),
+	// new UglifyJsPlugin(), // Replaced the reference to webpack.optimize.UglifyJsPlugin
+	// new CompressionPlugin({
+	// 	asset: '[path].gz[query]',
+	// 	algorithm: 'gzip',
+	// 	test: /\.(js|css|html|json|ico|svg|eot|otf|ttf)$/,
+	// }),
+];
 
 plugins = plugins.concat(process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins);
 
