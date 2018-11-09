9635d14493f7f0fd6153bcb0c4050f2cd135b7a8
Steven Choi <steven.m.choi@gmail.com>
2018-11-08 23:50:57 -0800

    Add footer-content elements

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -10,20 +10,79 @@ const footerLinkLists = [
 ];
 
 const Footer = () => (
-	<footer>
-		{footerLinkLists.map((list) => (
-			<ul className="link-col">
-				{list.map((link) => {
-					return (
-						<li>
-							<a className="footer-dead-link" href="" onClick={preventScroll}>
-								{link}
-							</a>
-						</li>
-					);
-				})}
-			</ul>
-		))}
+	<footer className="footer">
+		<div className="footer-content">
+			<div className="social">
+				<h3>FOLLOW US</h3>
+
+				<ul className="social-icons">
+					<li>
+						<a className="fb footer-dead-link" href="#" onClick={preventScroll} />
+					</li>
+
+					<li>
+						<a className="tw footer-dead-link" href="#" onClick={preventScroll} />
+					</li>
+
+					<li>
+						<a className="ig footer-dead-link" href="#" onClick={preventScroll} />
+					</li>
+
+					<li>
+						<a className="pin footer-dead-link" href="#" onClick={preventScroll} />
+					</li>
+				</ul>
+			</div>
+
+			<div className="newsletter">
+				<h3>DISCOVER WHAT'S COOKIN'</h3>
+
+				<form className="newsletter-form">
+					<input type="text" placeholder="Email Address" />
+					<button className="btn footer-dead-link" value="submit" onSubmit={preventScroll}>
+						GO
+					</button>
+				</form>
+
+				<p>Sign up for offers, recipes, news &amp; more</p>
+			</div>
+
+			<div className="blog">
+				<h3>
+					<a className="blog-header footer-dead-link" href="#" onClick={preventScroll}>
+						FROM THE BLOG
+					</a>
+				</h3>
+
+				<div className="blog-content">
+					<a className="blog-content-img footer-dead-link" href="#" onClick={preventScroll}>
+						<img src="https://upload.wikimedia.org/wikipedia/commons/6/63/First-kitchen_Fast_food_%2802%29.jpg" />
+					</a>
+
+					<h4 className="blog-content-text">
+						<a className="blog-content-text-link footer-dead-link" href="#" onClick={preventScroll}>
+							Grease Munchy now serving in China!
+						</a>
+					</h4>
+				</div>
+			</div>
+		</div>
+
+		<div className="footer-bottom">
+			{footerLinkLists.map((list) => (
+				<ul className="link-col">
+					{list.map((link) => {
+						return (
+							<li>
+								<a className="footer-dead-link" href="#" onClick={preventScroll}>
+									{link}
+								</a>
+							</li>
+						);
+					})}
+				</ul>
+			))}
+		</div>
 	</footer>
 );
 
