a0b34d40525afb8a97ad75eae93a8e851bdc0116
Steven Choi <steven.m.choi@gmail.com>
2018-11-09 03:08:55 -0800

    Remove default plugins for webpack4

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
 const path = require('path');
 const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 let plugins = []; // if using any plugins for both dev and production
 let devPlugins = []; // if using any plugins for development
 
-let prodPlugins = [
-	new webpack.DefinePlugin({
-		'process.env': {
-			NODE_ENV: JSON.stringify('production'),
-		},
-	}),
-	// new webpack.optimize.UglifyJsPlugin({
-	// 	compress: {
-	// 		warnings: true,
-	// 	},
-	// }),
+// let prodPlugins = [
+// new webpack.DefinePlugin({
+// 	'process.env': {
+// 		NODE_ENV: JSON.stringify('production'),
+// 	},
+// }),
+// new webpack.optimize.UglifyJsPlugin({
+// 	compress: {
+// 		warnings: true,
+// 	},
+// }),
 
-	new UglifyJsPlugin(), // Replaced the reference to webpack.optimize.UglifyJsPlugin
+// new UglifyJsPlugin(), // Replaced the reference to webpack.optimize.UglifyJsPlugin
 
-	// new CompressionPlugin({
-	// 	asset: '[path].gz[query]',
-	// 	algorithm: 'gzip',
-	// 	test: /\.(js|css|html|json|ico|svg|eot|otf|ttf)$/,
-	// }),
-];
+// new CompressionPlugin({
+// 	asset: '[path].gz[query]',
+// 	algorithm: 'gzip',
+// 	test: /\.(js|css|html|json|ico|svg|eot|otf|ttf)$/,
+// }),
+// ];
 
 plugins = plugins.concat(process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins);
 
 // include plugins config
 module.exports = {
-	mode: 'production',
+	mode: 'development',
 	context: __dirname,
 	entry: './frontend/entry.jsx',
 	output: {
