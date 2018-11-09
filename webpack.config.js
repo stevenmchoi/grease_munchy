696075bd5115cc574a06a1c5b131522116654aad
Steven Choi <steven.m.choi@gmail.com>
2018-11-09 03:36:09 -0800

    Clean up webpack config after succesful push to Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,9 @@
 const path = require('path');
-const webpack = require('webpack');
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 let plugins = []; // if using any plugins for both dev and production
 let devPlugins = []; // if using any plugins for development
 
-let prodPlugins = [
-	// new webpack.DefinePlugin({
-	// 	'process.env': {
-	// 		NODE_ENV: JSON.stringify('production'),
-	// 	},
-	// }),
-	// new webpack.optimize.UglifyJsPlugin({
-	// 	compress: {
-	// 		warnings: true,
-	// 	},
-	// }),
-	// new UglifyJsPlugin(), // Replaced the reference to webpack.optimize.UglifyJsPlugin
-	// new CompressionPlugin({
-	// 	asset: '[path].gz[query]',
-	// 	algorithm: 'gzip',
-	// 	test: /\.(js|css|html|json|ico|svg|eot|otf|ttf)$/,
-	// }),
-];
+let prodPlugins = [];
 
 plugins = plugins.concat(process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins);
 
@@ -71,7 +52,4 @@ module.exports = {
 	resolve: {
 		extensions: ['.js', '.jsx', '.svg', '*'],
 	},
-	// optimization: {
-	// 	minimizer: [new UglifyJsPlugin()],
-	// },
 };
